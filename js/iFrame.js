ebcb16938941f521cfa2f35d3a5d785e6be01e05
Jona <210352@student.glu.nl>
2023-12-08 16:11:44 +0200

    Update skills an about me

--- js/iFrame.js
+++ js/iFrame.js
@@ -1,25 +1,42 @@
-        var isExternalPageOpen = false;
+var isExternalPageOpen = false;
         var iframe;
 
-        function toggleExternalPage() {
+        function toggleExternalPage(String) {
             // Get the container element
             var container = document.getElementById('iframeContainer');
 
             // If the external page is open, close it
             if (isExternalPageOpen) {
-                container.innerHTML = '';
+                container.style.display = 'none';
                 isExternalPageOpen = false;
             } else {
                 // Create an iframe element
                 iframe = document.createElement('iframe');
 
-                // Set the source of the external website
-                iframe.src = 'https://jonasaija.github.io/Portfolio/unityc';
+                // Set the source of the external website to a local HTML file
+                iframe.src = String;
 
                 // Append the iframe to the container
                 container.innerHTML = '';
                 container.appendChild(iframe);
 
+                container.style.display = 'block';
                 isExternalPageOpen = true;
             }
+        }
+
+        // Event listener for the "Escape" key
+        document.addEventListener('keydown', function(event) {
+            if (event.key === 'Escape') {
+                closeExternalPage();
+            }
+        });
+
+        function closeExternalPage() {
+            // Get the container element
+            var container = document.getElementById('iframeContainer');
+
+            // Close the external page
+            container.style.display = 'none';
+            isExternalPageOpen = false;
         }
